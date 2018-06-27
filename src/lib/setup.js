460358546426c0f75eb77362b8594dbd46adc69c
Josep Servat <jservatlorca@gmail.com>
2018-06-27 02:02:30 +0200

    preparing setup

---
+++ src/lib/setup.js
@@ -0,0 +1,17 @@
+// request token 
+// request db password 
+// check database if exist
+// check database if not exist 
+// check if table exists 
+// create ledger table 
+
+var prompt = require('prompt-sync')();
+
+async cons setup = () => {
+    // perform setup tasks
+    
+}
+
+module.exports = {
+    setup
+}
