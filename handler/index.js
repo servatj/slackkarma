a9cb706fe57b8cb73fe471761185623a7805dee7
Josep <jservatlorca@gmail.com>
2019-09-20 00:57:04 +0200

    Remove Console log

--- handler/index.js
+++ handler/index.js
@@ -12,7 +12,6 @@ const getUsersCommands = (message) => {
 
 const handleMessage = ({ text, user }) => {
   const usersCommands = getUsersCommands(text)
-  console.log(usersCommands)
   return usersCommands
 }
 
